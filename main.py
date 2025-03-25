0129888ed2dfcf051264a673bb73c39d7eb3c00b
Vadim <frappz06@gmail.com>
2025-03-25 14:56:54 +0300

    Some changes

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(notifications.router)
 
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
